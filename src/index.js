9c75a5d2e66e84deae7ea88f4d409dc2e14fc2af
madMarkus <soc.g.SMA@gmail.com>
2018-05-26 21:39:46 +0300

    added local fonts support

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 
 import RootContainer from './containers/RootContainer';
 
+import './assets/fonts/index.css';
+
 ReactDOM.render(<RootContainer />, document.getElementById('app'));
