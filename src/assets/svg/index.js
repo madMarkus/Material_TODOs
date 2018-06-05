a6db21f166407362f208d440559756c5a8241094
madMarkus <soc.g.SMA@gmail.com>
2018-06-05 17:49:02 +0300

    Swap to dark-citrus theme

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -12,21 +12,21 @@ const wrapSvgPath = (path, viewBox = '0 0 24 24') => props => {
 
 const CheckBoxBlankPath = (
   <path
-    fill="#757575"
+    fill="#fff"
     d="M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M19,5V19H5V5H19Z"
   />
 );
 
 const CheckBoxMarkedPath = (
   <path
-    fill="#f50057"
+    fill="#76FF03"
     d="M10,17L5,12L6.41,10.58L10,14.17L17.59,6.58L19,8M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3Z"
   />
 );
 
 const ClosePath = (
   <path
-    fill="#757575"
+    fill="#F44336"
     d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
   />
 );
--- src/containers/RootContainer.js
+++ src/containers/RootContainer.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import MainContainer from './MainContainer';
 import CssBaseline from 'material-ui/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 
 import store from '../store';
 import { Provider } from 'react-redux';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#76FF03'
+    }
+  }
+});
 
 class RootContainer extends Component {
   render() {
@@ -11,7 +21,9 @@ class RootContainer extends Component {
       <Provider store={store}>
         <React.Fragment>
           <CssBaseline />
-          <MainContainer />
+          <MuiThemeProvider theme={theme}>
+            <MainContainer />
+          </MuiThemeProvider>
         </React.Fragment>
       </Provider>
     );
